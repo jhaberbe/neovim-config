84ae5abcd38d6efb556714e39b260e07e1a36bee
jhaberbe <jhaberbe@stanford.edu>
2025-05-01 15:31:03 -0700

    Added everything for magma. Still not working.

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -67,16 +67,10 @@ return {
     },
 
     {
-	'MeanderingProgrammer/render-markdown.nvim',
-	dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, 
-	-- if you use the mini.nvim suite
-	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
-	-- if you use standalone mini plugins
-	-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, 
-	-- if you prefer nvim-web-devicons
-	---@module 'render-markdown'
-	---@type render.md.UserConfig
-	opts = {},
+	"nvim-neorg/neorg",
+	lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+	version = "*", -- Pin Neorg to the latest stable release
+	config = true,
     },
 
     {
@@ -90,5 +84,9 @@ return {
 	"ibhagwan/fzf-lua",              -- optional
 	"echasnovski/mini.pick",         -- optional
       },
+    },
+    
+    {
+	'dccsillag/magma-nvim', 
     }
 }
